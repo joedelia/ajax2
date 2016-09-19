b143e4717dfd44e144ed9b43ba111dd71aa4ce10
Giordano Bruno d'Elia <joe@joedelia.me>
2016-09-19 20:20:56 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import $ from 'jquery'
+import _ from 'lodash'
+import {toCamelCase, toSnakeCase} from 'case-converter'
+
+function request(method, url, data, {convertRequest, convertResponse}) {
+    return new Promise((resolve, reject) => {
+        if (data) {
+            if (convertRequest == 'camelCase') {
+                data = toCamelCase(data);
+            } else if (convertRequest == 'snakeCase') {
+                data = toSnakeCase(data);
+            }
+        }
+        $.ajax({
+            method: method,
+            url: url,
+            data: data,
+            success: (body) => {
+                switch (convertResponse) {
+                    case 'snakeCase':
+                        resolve(_.isObject(body) ? toSnakeCase(body) : body);
+                        break;
+                    case 'camelCase':
+                        resolve(_.isObject(body) ? toCamelCase(body) : body);
+                        break;
+                    default:
+                        resolve(body);
+                        break;
+                }
+            },
+            error: function (jqXHR, jqError, error) {
+                reject({
+                    code: jqXHR.status,
+                    error: error
+                });
+            }
+        });
+    });
+}
+
+var exp = {};
+
+for (let method of ['get', 'post', 'put', 'patch', 'delete']) {
+    exp[method] = function (url, data = null, {convertRequest = 'snakeCase', convertResponse = 'camelCase'} = {}) {
+        return request(method, url, data, {convertRequest, convertResponse});
+    }
+}
+
+export default exp;
