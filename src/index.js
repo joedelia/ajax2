1a0c064fb249a0c49e37019c7244f4bf588f108b
Giordano Bruno d'Elia <joe@joedelia.me>
2016-09-28 12:10:08 +0100

    Use 'fetch'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import $ from 'jquery'
-import _ from 'lodash'
+import 'whatwg-fetch';
+import isObject from 'lodash.isobject'
 import {toCamelCase, toSnakeCase} from 'case-converter'
 
-var defaultConvertRequest,
-    defaultConvertResponse;
+var defaultConvertRequest = 'snakeCase',
+    defaultConvertResponse = 'camelCase',
+    defaultHeaders = {},
+    defaultCredentials = 'same-origin';
 
-function configure({convertRequest = 'snakeCase', convertResponse = 'camelCase', headers = {}} = {}) {
+function configure({convertRequest = 'snakeCase', convertResponse = 'camelCase', headers = {}, credentials = 'same-origin'} = {}) {
     defaultConvertRequest = convertRequest;
     defaultConvertResponse = convertResponse;
-    if(!_.isEmpty(headers)) {
-        $.ajaxSetup({
-            headers: headers
-        });
+    defaultHeaders = headers;
+    defaultCredentials = credentials;
+}
+
+function parseResponse(body, convertResponse) {
+    if (response.headers.get('Content-Type') == 'application/json') {
+        switch (convertResponse) {
+            case 'snakeCase':
+                return isObject(body.json()) ? toSnakeCase(body.json()) : body.json();
+                break;
+            case 'camelCase':
+                return isObject(body.json()) ? toCamelCase(body.json()) : body.json();
+                break;
+            default:
+                return body.json();
+                break;
+        }
+    } else {
+        return body.text();
+    }
+}
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        error.status = status;
+        throw error;
     }
 }
 
@@ -24,38 +52,41 @@ function request(method, url, data, {convertRequest, convertResponse}) {
                 data = toSnakeCase(data);
             }
         }
-        $.ajax({
+        var headers = defaultHeaders;
+        if (data) {
+            headers['Content-Type'] = 'application/json';
+        }
+        fetch(url, {
             method: method,
             url: url,
-            data: data,
-            success: (body) => {
-                switch (convertResponse) {
-                    case 'snakeCase':
-                        resolve(_.isObject(body) ? toSnakeCase(body) : body);
-                        break;
-                    case 'camelCase':
-                        resolve(_.isObject(body) ? toCamelCase(body) : body);
-                        break;
-                    default:
-                        resolve(body);
-                        break;
-                }
-            },
-            error: function (jqXHR, jqError, error) {
+            headers: headers,
+            credentials: defaultCredentials,
+            body: data ? JSON.stringify(data) : null
+        })
+            .then(checkStatus)
+            .then(function (response) {
+                resolve(parseResponse(response, convertResponse));
+            })
+            .catch(function (error) {
                 reject({
-                    code: jqXHR.status,
-                    error: error
+                    code: error.status,
+                    error: error.message,
+                    response: parseResponse(error.response, convertResponse)
                 });
-            }
-        });
+            });
     });
 }
 
 var exp = {};
 
 for (let method of ['get', 'post', 'put', 'patch', 'delete']) {
-    exp[method] = function (url, data = null, {convertRequest = 'snakeCase', convertResponse = 'camelCase'} = {}) {
-        return request(method, url, data, {convertRequest, convertResponse});
+    exp[method] = function (url,
+                            data = null,
+                            {
+                                convertRequest = defaultConvertRequest || 'snakeCase',
+                                convertResponse = defaultConvertResponse || 'camelCase'
+                            } = {}) {
+        return request(method.toUpperCase(), url, data, {convertRequest, convertResponse});
     }
 }
 
